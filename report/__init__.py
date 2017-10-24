3e905188ff2b27af981fc3c61c9fbe7e7c614a97
Ethan Furman <ethan@stoneleaf.us>
2017-10-24 14:00:22 -0700

    do not change values (only domains) in onload() method

--- report/__init__.py
+++ report/__init__.py
@@ -1,13 +1,14 @@
 from antipathy import Path as _Path
 from xaml import Xaml as _Xaml
 
-report_name = (_Path(__file__).path or _Path('.'))/'sample.xaml'
-with open(report_name) as src:
-    xaml_doc = _Xaml(src.read()).document
+src_dir = _Path(__file__).path or _Path('.')
 
-if len(xaml_doc.pages) != 2:
-    raise ValueError('%s should have an xml and an xsl component' % report_name)
+for report_name in src_dir.glob('*.xaml'):
+    with open(report_name) as src:
+        xaml_doc = _Xaml(src.read()).document
+        if len(xaml_doc.pages) != 2:
+            raise ValueError('%s should have an xml and an xsl component' % report_name)
+        for page in xaml_doc.pages:
+            with open(report_name.strip_ext() + '.' + page.ml.type, 'wb') as dst:
+                dst.write(page.bytes())
 
-for page in xaml_doc.pages:
-    with open(report_name.strip_ext() + '.' + page.ml.type, 'wb') as dst:
-        dst.write(page.bytes())
